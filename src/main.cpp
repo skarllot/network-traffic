f1c64c0a660fdadf6fbb27694fba0b5f664da1e4
Fabrício Godoy <skarllot@gmail.com>
2009-10-23 23:36:24 -0200

    Performance test between stat() and opendir()

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <time.h>
+
+#include <dirent.h>
+
 int main()
 {
-    std::cout << "Hello world" << std::endl;
+    time_t a_begin = time(NULL);
+    for (int a = 0; a < 10000000; a++)
+    {
+        struct stat buf;
+        int ret = stat("/home/fabricio", &buf);
+        ret = stat("/home/nouser", &buf);
+        //std::cout << a << std::endl;
+    }
+    time_t a_end = time(NULL);
+    std::cout << "Test A took " << (a_end - a_begin) << " seconds" << std::endl;
+
+    time_t b_begin = time(NULL);
+    for (int b=0; b<10000000; b++)
+    {
+        DIR* dirp;
+        dirp = opendir("/home/fabricio");
+        dirp = opendir("/home/nouser");
+        //std::cout << b << std::endl;
+    }
+    time_t b_end = time(NULL);
+    std::cout << "Test B took " << (b_end - b_begin) << " seconds" << std::endl;
+
     return 0;
 }
