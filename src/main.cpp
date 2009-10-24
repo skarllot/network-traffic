995f14a21e5d2a0b9c56ca09a722cb0df90a0a8b
Fabrício Godoy <skarllot@gmail.com>
2009-10-24 01:36:29 -0200

    Changed package name to 'testcase'
    Changed package name and binary name to 'testcase'.
    Some code cleanup.

--- src/main.cpp
+++ src/main.cpp
@@ -16,18 +16,16 @@ int main()
         struct stat buf;
         int ret = stat("/home/fabricio", &buf);
         ret = stat("/home/nouser", &buf);
-        //std::cout << a << std::endl;
     }
     time_t a_end = time(NULL);
     std::cout << "Test A took " << (a_end - a_begin) << " seconds" << std::endl;
 
     time_t b_begin = time(NULL);
-    for (int b=0; b<10000000; b++)
+    for (int b = 0; b < 10000000; b++)
     {
         DIR* dirp;
         dirp = opendir("/home/fabricio");
         dirp = opendir("/home/nouser");
-        //std::cout << b << std::endl;
     }
     time_t b_end = time(NULL);
     std::cout << "Test B took " << (b_end - b_begin) << " seconds" << std::endl;
